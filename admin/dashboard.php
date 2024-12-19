2d007a464bcaf9b253e87e984a27788034be771b
Mouhamed-Talibi <mouha22talibi@gmail.com>
2024-12-19 22:11:11 +0100

    Updating Admin Dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,5 +1,28 @@
 <?php
     require_once "include/admin_session.php";
+    require_once "../database/config.php";
+
+    // admin infos
+    if(isset($_SESSION['admin_id'])){
+        $query  = mysqli_prepare($connect, "SELECT * FROM admins WHERE id=?");
+        mysqli_stmt_bind_param($query, "i", $_SESSION['admin_id']);
+        mysqli_stmt_execute($query);
+        $result = mysqli_stmt_get_result($query);
+        $admin  = mysqli_fetch_assoc($result);
+    }
+
+    // totale products :
+    $productsQuery = mysqli_prepare($connect, "SELECT id FROM products");
+    mysqli_stmt_execute($productsQuery);
+    $result = mysqli_stmt_get_result($productsQuery);
+    $products_rows = mysqli_num_rows($result);
+
+    // totale categories :
+    $categoriesQuery = mysqli_prepare($connect, "SELECT id FROM categories");
+    mysqli_stmt_execute($categoriesQuery);
+    $result = mysqli_stmt_get_result($categoriesQuery);
+    $categories_rows = mysqli_num_rows($result);
+
     // set title
     $title = "Admin | Dashboard";
     $stylePath = "style/dashboard.css";
@@ -9,7 +32,7 @@
     <!-- content -->
     <main>
         <div class="message">
-            <h2>Welcome Back, <span>Mouhamed Talibi</span>!</h2>
+            <h2>Welcome Back, <span><?= $admin['username']?></span>!</h2>
             <br>
             <p>
                 Would you like to: 
@@ -21,20 +44,20 @@
 
         <div class="statics">
             <div class="card">
-                <div class="card-title">Total Orders:</div>
-                <div class="card-description"><span>190</span> Order</div>
+                <div class="card-title">Totale Products :</div>
+                <div class="card-description"><span><?= $products_rows?></span> Product</div>
             </div>
             <div class="card">
-                <div class="card-title">Orders Paid:</div>
-                <div class="card-description"><span>178</span> Order</div>
+                <div class="card-title">Totale Categories:</div>
+                <div class="card-description"><span><?= $categories_rows ?></span> category</div>
             </div>
             <div class="card">
-                <div class="card-title">Orders Canceled:</div>
+                <div class="card-title">Totale Orders :</div>
                 <div class="card-description"><span>67</span> Order</div>
             </div>
             <div class="card">
-                <div class="card-title">Total Amount:</div>
-                <div class="card-description"><span>$15,564</span></div>
+                <div class="card-title">Cancled Orders :</div>
+                <div class="card-description"><span>68</span> Orders</div>
             </div>
         </div>
     </main> 
